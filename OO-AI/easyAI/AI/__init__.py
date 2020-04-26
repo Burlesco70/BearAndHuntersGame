bd9810923ec5c2fb1f967abaa6ba921c51af039c
Burlesco70 <mario.nardi.70@gmail.com>
2020-04-26 17:44:53 +0200

    Removing unuseful files

--- OO-AI/easyAI/AI/__init__.py
+++
@@ -1,8 +0,0 @@
-from .Negamax import Negamax
-from .NonRecursiveNegamax import NonRecursiveNegamax
-from .TT import TT
-from .solving import id_solve, df_solve
-from .MTdriver import mtd
-from .SSS import SSS
-from .DUAL import DUAL
-from .HashTT import HashTT
